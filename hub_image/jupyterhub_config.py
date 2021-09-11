5e6ca36ef662d3c8b814f591e0f37a658fe9957a
Phil Austin <paustin@eoas.ubc.ca>
2021-09-11 20:28:32 +0000

    eoas05

--- hub_image/jupyterhub_config.py
+++ hub_image/jupyterhub_config.py
@@ -1,22 +1,21 @@
 # dummy for testing. Don't use this in production!
-#c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 
 # launch with docker
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.JupyterHub.authenticator_class = 'firstuseauthenticator.FirstUseAuthenticator'
-c.Authenticator.allowed_users = {'test1','test2','phil','aloeppky','fjones','cjohnson','czhang','hari'}
+# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+# c.JupyterHub.authenticator_class = 'firstuseauthenticator.FirstUseAuthenticator'
+# c.Authenticator.allowed_users = {'test1','test2','phil','aloeppky','fjones','cjohnson','czhang','hari'}
 c.Authenticator.admin_users = {'test1','test2','phil','aloeppky','fjones','cjohnson','czhang','hari'}
 # we need the hub to listen on all ips when it is in a container
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
-c.JupyterHub.hub_connect_ip = 'newe211hub'
+c.JupyterHub.hub_connect_ip = 'e211hub'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
 # c.DockerSpawner.image = 'phaustin/basicbook:aug07'
-c.DockerSpawner.allowed_images = {'e211' : 'phaustin/basicbook:aug07',
-                                  'e350' : 'phaustin/basicbook:aug07'}
+c.DockerSpawner.allowed_images = {'e211' : 'phaustin/e211book:sep11'}
 notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
@@ -32,3 +31,4 @@ c.DockerSpawner.volumes = {"e211hub-user-{username}": notebook_dir}
 
 # delete containers when the stop
 c.DockerSpawner.remove = True
+
